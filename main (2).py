eeb8fb5a1a524b734d279e5fb0b58508c66a75c4
Pavithra2689 <144588194+Pavithra2689@users.noreply.github.com>
2023-09-13 21:51:20 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,10 +1,6 @@
-year = 2000
+year = 2003
 
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-
+if (year%400 == 0) or (year%4==0 and year%100!=0):
+    print("Leap Year")
 else:
-    print("{0} is not a leap year".format(year))
+    print("Not a Leap Year")
